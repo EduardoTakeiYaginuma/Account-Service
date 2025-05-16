8ce0256b7688e26d6dd5bd1d268ee1aa53388da0
EduardoTakeiYaginuma <eduyagnuma10@gmail.com>
2025-05-16 14:38:01 -0300

    dev

---
+++ src/main/resources/db/migration/V2025.02.21.002__create_table_account.sql
@@ -0,0 +1,14 @@
+CREATE TABLE account (
+    id_account VARCHAR(36) NOT NULL,
+    tx_name VARCHAR(256) NOT NULL,
+    tx_email VARCHAR(256) NOT NULL,
+    tx_sha256 VARCHAR(64) NOT NULL,
+    dt_birthdate DATE NULL,
+    dt_creation TIMESTAMP NOT NULL,
+    CONSTRAINT pk_account PRIMARY KEY (id_account)
+);
+
+CONSTRAINT fk_orders_account
+    FOREIGN KEY (id_account)
+    REFERENCES account(id_account)  -- ou REFERENCES account(id) se o PK lá for id
+    ON DELETE CASCADE    
